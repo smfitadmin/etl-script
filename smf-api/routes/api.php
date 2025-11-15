fc3cc91a94401643a28edb00e1a739ddcf1e624a
smfitadmin <itadmin@selfmade.finance>
2025-11-15 22:46:26 +0700

    add new endpoint for bol api

--- smf-api/routes/api.php
+++ smf-api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PublicApiController;
 use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\BolApiController;
 use Illuminate\Http\Request;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -16,6 +17,7 @@ Route::get('/ping', function () {
 });
 
 Route::prefix('/public')->group(function () {
+    Route::post('/bol-store', [BolApiController::class, 'bol_store']);
     Route::post('/bol-bs', [PublicApiController::class, 'bol_bs_store']);
     Route::post('/bol-ic', [PublicApiController::class, 'bol_ic_store']);
 
@@ -27,7 +29,7 @@ Route::prefix('/public')->group(function () {
     Route::post('/dbd-supplier', [PublicApiController::class, 'dbd_supplier_store']);
     Route::post('/dbd-company-supplier', [PublicApiController::class, 'dbd_company_supplier_store']);
     Route::get('/directors/{registered_no}', [PublicApiController::class, 'directorsByRegisteredNo']);
-    Route::get('/company/{tax_id}/financial', [\App\Http\Controllers\PublicApiController::class, 'getCompanyFinancialAllYears']);
+    Route::get('/company/{tax_id}/financial', [PublicApiController::class, 'getCompanyFinancialAllYears']);
     Route::get('/company/{tax_id}/financial/{year}', [PublicApiController::class, 'getCompanyFinancial']);
 });
 
